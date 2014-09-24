18c86da517e2eeb8c4fc6fd9032572e9279a9490
Baiqiang <qiyuuu@gmail.com>
2014-09-24 14:30:16 +0800

    Change mail back to qq, change large number of bcc into individual email

--- protected/extensions/mail/views/competitionNotice.php
+++ protected/extensions/mail/views/competitionNotice.php
@@ -5,6 +5,7 @@ $style = 'padding-top:13px;padding-left:39px;padding-right:13px;padding-bottom:1
 	<td style="<?php echo $style; ?>">
 		<p><strong>亲爱的<i><?php echo $competition->name_zh; ?></i>参赛者：</strong></p>
 		<?php echo $content; ?>
+		<p>本邮件由系统代发，如有疑问，请联系主办方：<br><?php echo implode('<br>', $organizers); ?>。</p>
 	</td>
 <tr>
 <?php if (trim(strip_tags($englishContent, '<img>')) != ''): ?>
@@ -12,6 +13,7 @@ $style = 'padding-top:13px;padding-left:39px;padding-right:13px;padding-bottom:1
 	<td style="<?php echo $style; ?>">
 		<p>Dear competitior in <i><?php echo $competition->name; ?></i>,</p>
 		<?php echo $englishContent; ?>
+		<p>This is a system-sent Email. If you have any question, you can contact<br><?php echo implode('<br>', $organizers); ?>.</p>
 	</td>
 </tr>
 <?php endif; ?>
