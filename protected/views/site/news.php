9f6a5acf547c3ffa67ca7b8bbf64e610c3f8d079
Baiqiang <qiyuuu@gmail.com>
2015-06-12 00:45:20 +0800

    Change style of top news

--- protected/views/site/news.php
+++ protected/views/site/news.php
@@ -1,4 +1,19 @@
-<article class="events-item page-row row has-divider clearfix">
+<article class="events-item row clearfix<?php if ($data->weight == 0) echo ' has-divider page-row'; ?>">
+  <?php if ($data->weight > 0): ?>
+  <div class="panel panel-theme">
+    <div class="panel-heading">
+      <h3 class="panel-title"><?php echo CHtml::encode($data->getAttributeValue('title')); ?></h3>
+    </div>
+    <div class="panel-body">
+      <p class="meta">
+        <span class="date"><i class="fa fa-calendar"></i> <?php echo date('Y-m-d', $data->date); ?></span>
+        <span class="time"><i class="fa fa-clock-o"></i> <?php echo date('H:i', $data->date); ?></span>
+        <span class="author"><i class="fa fa-user"></i> <?php echo $data->user->getAttributeValue('name', true); ?></span>
+      </p>  
+      <div class="desc"><?php echo $data->getAttributeValue('content'); ?></div>
+    </div>
+  </div>
+  <?php else: ?>
   <div class="details col-xs-12">
     <h3 class="title"><?php echo CHtml::encode($data->getAttributeValue('title')); ?></h3>
     <p class="meta">
@@ -8,4 +23,5 @@
     </p>  
     <div class="desc"><?php echo $data->getAttributeValue('content'); ?></div>
   </div><!--//details-->
+  <?php endif; ?>
 </article>
