9775abb7f6b4ff3f235d010095f4285e25bd7f89
Baiqiang <qiyuuu@gmail.com>
2015-06-24 19:37:10 +0800

    Add solves and attempts to personal page

--- protected/models/wca/Persons.php
+++ protected/models/wca/Persons.php
@@ -140,12 +140,24 @@ class Persons extends ActiveRecord {
 		$command = $db->createCommand();
 		$command->select(array(
 			'eventId',
-			'sum(CASE WHEN pos=1 THEN 1 ELSE 0 END) AS gold',
-			'sum(CASE WHEN pos=2 THEN 1 ELSE 0 END) AS silver',
-			'sum(CASE WHEN pos=3 THEN 1 ELSE 0 END) AS bronze',
+			'sum(CASE WHEN pos=1 AND roundId IN ("c", "f") AND best>0 THEN 1 ELSE 0 END) AS gold',
+			'sum(CASE WHEN pos=2 AND roundId IN ("c", "f") AND best>0 THEN 1 ELSE 0 END) AS silver',
+			'sum(CASE WHEN pos=3 AND roundId IN ("c", "f") AND best>0 THEN 1 ELSE 0 END) AS bronze',
+			'sum(CASE WHEN value1>0 THEN 1 ELSE 0 END)
+			+sum(CASE WHEN value2>0 THEN 1 ELSE 0 END)
+			+sum(CASE WHEN value3>0 THEN 1 ELSE 0 END)
+			+sum(CASE WHEN value4>0 THEN 1 ELSE 0 END)
+			+sum(CASE WHEN value5>0 THEN 1 ELSE 0 END)
+			AS solve',
+			'sum(CASE WHEN value1>-2 AND value1!=0 THEN 1 ELSE 0 END)
+			+sum(CASE WHEN value2>-2 AND value2!=0 THEN 1 ELSE 0 END)
+			+sum(CASE WHEN value3>-2 AND value3!=0 THEN 1 ELSE 0 END)
+			+sum(CASE WHEN value4>-2 AND value4!=0 THEN 1 ELSE 0 END)
+			+sum(CASE WHEN value5>-2 AND value5!=0 THEN 1 ELSE 0 END)
+			AS attempt',
 		))
 		->from('Results')
-		->where('personId=:personId AND roundId IN ("c", "f") AND best>0', array(
+		->where('personId=:personId', array(
 			':personId'=>$id,
 		))
 		->group('eventId');
