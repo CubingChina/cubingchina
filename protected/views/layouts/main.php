0a36a07b740796a5c2df1299d975a6ffc7430b8d
Baiqiang <qiyuuu@gmail.com>
2015-09-11 17:30:26 +0800

    Add logo for english

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -3,7 +3,7 @@
   <header class="header">
     <div class="header-main container">
       <h1 class="logo col-md-4 col-sm-4">
-          <a href="<?php echo $this->createUrl('/site/index'); ?>"><img id="logo" src="/f/images/logo.png" alt="Logo"></a>
+          <a href="<?php echo $this->createUrl('/site/index'); ?>"><img id="logo" src="/f/images/logo<?php if (Yii::app()->language == 'en') echo '-en'; ?>.png" alt="Logo"></a>
       </h1><!--//logo-->
       <div class="info col-md-8 col-sm-8 hidden-xs">
         <ul class="menu-top navbar-right">
