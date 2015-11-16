1204e67d48ad9be20029e9a7fa4fce5cb51375f1
Baiqiang <qiyuuu@gmail.com>
2015-11-16 17:18:12 +0800

    Fix bug

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -316,8 +316,6 @@ class Controller extends CController {
 			$this->setLanguage($_REQUEST['lang'], true);
 		} else if(isset($_COOKIE['language']) && $_COOKIE['language'] != '') {
 			$this->setLanguage($_COOKIE['language']);
-		} else if(Yii::app()->session->get('language') !== null) {
-			$this->setLanguage(Yii::app()->session->get('language'), true);
 		} else if(isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])){
 			$languages = Yii::app()->params->languages;
 			$acceptLanguage = strtolower(str_replace('-', '_', $_SERVER['HTTP_ACCEPT_LANGUAGE']));
@@ -342,10 +340,9 @@ class Controller extends CController {
 			return;
 		}
 		Yii::app()->language = $language;
-		Yii::app()->session->add('language', $language);
 		if ($setCookie) {
 			$_COOKIE['language'] = $language;
-			setcookie('language', $language, time() + 365 * 86400, '/');
+			setcookie('language', $language, time() + 365 * 86400, '/', DEV ? null : '.cubingchina.com');
 		}
 	}
 
