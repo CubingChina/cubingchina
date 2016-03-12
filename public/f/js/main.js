93fec0ef060b72308f499a837368a20c37238ca2
Baiqiang <qiyuuu@gmail.com>
2016-03-13 01:31:57 +0800

    Fix statistics script loading

--- public/f/js/main.js
+++ public/f/js/main.js
@@ -167,7 +167,7 @@ $(function() {
       dd.height(104).css('overflow-y', 'hidden');
     }
   })
-  if (location.hostname === 'cubingchina.com'){
+  if (location.hostname.indexOf('cubingchina.com') > -1){
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
