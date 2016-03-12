932066fa804771768cee26498a81dfd31f863a7f
Baiqiang <qiyuuu@gmail.com>
2016-03-12 19:06:11 +0800

    Add wca regulations link to regulations section

--- protected/views/competition/regulations.php
+++ protected/views/competition/regulations.php
@@ -1,6 +1,14 @@
 <div class="col-lg-12 competition-<?php echo strtolower($competition->type); ?>">
-	<dl>
-		<dt><?php echo Yii::t('Competition', 'Regulations'); ?></dt>
-		<dd><?php echo $competition->getAttributeValue('regulations'); ?></dd>
-	</dl>
+  <dl>
+    <?php if ($competition->type == Competition::TYPE_WCA): ?>
+    <dt><?php echo Yii::t('Competition', 'WCA Competition'); ?></dt>
+    <dd>
+      <?php echo Yii::t('Competition', 'This competition is recognized as an official World Cube Association competition. Therefore, all competitors should be familiar with the {regulations}.', array(
+      '{regulations}'=>CHtml::link(Yii::t('Competition', 'WCA regulations'), $competition->getWcaRegulationUrl(), array('target'=>'_blank')),
+    ));?>
+    </dd>
+    <?php endif; ?>
+    <dt><?php echo Yii::t('Competition', 'Regulations'); ?></dt>
+    <dd><?php echo $competition->getAttributeValue('regulations'); ?></dd>
+  </dl>
 </div>
