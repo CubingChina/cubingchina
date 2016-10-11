03b45db35943d89b86156d2250e815ffc0ab6a1d
Baiqiang <qiyuuu@gmail.com>
2016-10-11 19:44:42 +0800

    Change when to display buttons

--- protected/views/results/cert.php
+++ protected/views/results/cert.php
@@ -1,45 +1,50 @@
 <div class="col-lg-12 results-cert">
   <div class="text-center">
     <p>
+      <?php if ($this->user && $user->id == $this->user->id): ?>
       <?php echo CHtml::link(Yii::t('common', 'Download Result Certificate'), $cert->getImageUrl('results'), [
         'download'=>Yii::t('common', 'Result Certificate') . '.jpg',
         'target'=>'_blank',
         'class'=>'btn btn-theme',
       ]); ?>
-      <?php if ($cert->has_participations && $this->user && $user->id == $this->user->id): ?>
+      <?php if ($cert->has_participations): ?>
       <?php echo CHtml::link(Yii::t('common', 'Download Participation Certificate'), $cert->getImageUrl('participations'), [
         'download'=>Yii::t('common', 'Participation Certificate') . '.jpg',
         'target'=>'_blank',
         'class'=>'btn btn-success',
       ]); ?>
       <?php endif; ?>
-      <?php if ($this->user && $this->user->hasCerts): ?>
+      <?php endif; ?>
       <?php echo CHtml::link(Yii::t('common', 'My Certificates'), ['/user/cert'], [
         'class'=>'btn btn-info',
       ]); ?>
-      <?php endif; ?>
     </p>
     <p>
       <?php echo CHtml::image($cert->getImageUrl('results')); ?>
     </p>
+    <?php if ($cert->has_participations && $this->user && $user->id == $this->user->id): ?>
     <p>
+      <?php echo CHtml::image($cert->getImageUrl('participations')); ?>
+    </p>
+    <?php endif; ?>
+    <p>
+      <?php if ($this->user && $user->id == $this->user->id): ?>
       <?php echo CHtml::link(Yii::t('common', 'Download Result Certificate'), $cert->getImageUrl('results'), [
         'download'=>Yii::t('common', 'Result Certificate') . '.jpg',
         'target'=>'_blank',
         'class'=>'btn btn-theme',
       ]); ?>
-      <?php if ($cert->has_participations && $this->user && $user->id == $this->user->id): ?>
+      <?php if ($cert->has_participations): ?>
       <?php echo CHtml::link(Yii::t('common', 'Download Participation Certificate'), $cert->getImageUrl('participations'), [
         'download'=>Yii::t('common', 'Participation Certificate') . '.jpg',
         'target'=>'_blank',
         'class'=>'btn btn-success',
       ]); ?>
       <?php endif; ?>
-      <?php if ($this->user && $this->user->hasCerts): ?>
+      <?php endif; ?>
       <?php echo CHtml::link(Yii::t('common', 'My Certificates'), ['/user/cert'], [
         'class'=>'btn btn-info',
       ]); ?>
-      <?php endif; ?>
     </p>
   </div>
 </div>
