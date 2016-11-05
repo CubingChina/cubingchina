98dc8e6597ad5b953dec516676c30ed2941096ea
Baiqiang <qiyuuu@gmail.com>
2016-11-05 08:03:05 +0800

    Fix bug

--- protected/views/competition/operation.php
+++ protected/views/competition/operation.php
@@ -1,5 +1,6 @@
-<?php if ($this->user->isOrganizer() && isset($competition->organizers[$this->user->id])
-  || $this->user->isAdministrator()): ?>
+<?php if (!Yii::app()->user->isGuest
+  && ($this->user->isOrganizer() && isset($competition->organizers[$this->user->id])
+  || $this->user->isAdministrator())): ?>
 <div class="col-lg-12">
   <?php echo CHtml::link(Yii::t('common', 'Edit'), ['/board/competition/edit', 'id'=>$competition->id], [
     'class'=>'btn btn-xs btn-theme',
