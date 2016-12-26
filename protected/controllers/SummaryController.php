aad3549e2e4b7ad703ec6423e96204be2c0a6f43
Baiqiang <qiyuuu@gmail.com>
2016-12-27 00:27:47 +0800

    Add cache for summary

--- protected/controllers/SummaryController.php
+++ protected/controllers/SummaryController.php
@@ -23,6 +23,9 @@ class SummaryController extends Controller {
 	public function actionPerson() {
 		$year = $this->iGet('year');
 		$id = strtoupper($this->sGet('id'));
+		if ($year != 2016) {
+			throw new CHttpException(404);
+		}
 		$summaryClassName = 'Summary' . $year;
 		$summaryClass = new $summaryClassName();
 		$person = Persons::model()->with('country')->findByAttributes(['id' => $id]);
@@ -30,7 +33,7 @@ class SummaryController extends Controller {
 			throw new CHttpException(404);
 		}
 		$data = Yii::app()->cache->getData(['Persons', 'getResults'], $id);
-		$summary = $summaryClass->person($person, $data);
+		$summary = Yii::app()->cache->getData([$summaryClass, 'person'], [$person, $data]);
 		$this->title = Yii::t('common', '{personName}\'s {year} Annual Summary', [
 			'{personName}'=>$person->name,
 			'{year}'=>$year,
