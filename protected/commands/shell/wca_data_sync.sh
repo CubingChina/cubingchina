d907b6540d0f4637efe508be8333d9932184e9fa
Baiqiang <qiyuuu@gmail.com>
2017-01-03 18:11:23 +0800

    Add time info to the log

--- protected/commands/shell/wca_data_sync.sh
+++ protected/commands/shell/wca_data_sync.sh
@@ -1,12 +1,17 @@
 dir=`dirname "$0"`
 wca_home='https://www.worldcubeassociation.org'
+
+_log () {
+	echo "[`date +'%Y-%m-%d %H:%M:%S'`] $1"
+}
+
 cd $dir
 db_config="`dirname \`dirname \\\`pwd\\\`\``/config/wcaDb"
 db_num=`expr \( \`cat $db_config\` + 1 \) % 2`
 mysql_user='cubingchina'
 mysql_pass=''
 mysql_db="wca_$db_num"
-echo "get export data from wca"
+_log "get export data from wca"
 wget $wca_home/results/misc/export.html
 zipname=`grep -o '\(WCA_export[0-9]\+_[0-9]\{8\}\.sql\.zip\)' export.html | tail -1`
 
@@ -29,6 +34,10 @@ else
 	last_version=0
 	last_date=0
 fi
+_log "last_version: $last_version"
+_log "version: $version"
+_log "last_date: $last_date"
+_log "date: $date"
 if [ "$last_version" -ge "$version" ] && [ "$last_date" -ge "$date" ]
 then
 	rm -f export.html*
@@ -39,16 +48,16 @@ echo $version >> last
 echo $date >> last
 
 lftp -c "pget -n 20 '$wca_home/results/misc/$zipname' -o $zipname"
-echo "unzip the export data"
+_log "unzip the export data"
 unzip -o $zipname WCA_export.sql
-echo "replace charset to utf8_general_ci"
+_log "replace charset to utf8_general_ci"
 sed -ri 's/latin1/utf8/g' WCA_export.sql
 sed -ri 's/utf8_unicode_ci/utf8_general_ci/g' WCA_export.sql
-echo "import data"
+_log "import data"
 mysql --force --user=$mysql_user --password=$mysql_pass $mysql_db < WCA_export.sql
-echo "import additional"
+_log "import additional"
 mysql --user=$mysql_user --password=$mysql_pass $mysql_db < additional.sql
 rm -f export.html* WCA_export*
-echo "build some data and clean cache"
+_log "build some data and clean cache"
 echo -n $db_num > $db_config
 ../../yiic wca update
