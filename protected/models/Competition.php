3ec4e123f53f83ab91343b74b776199631fdfbbc
Baiqiang <qiyuuu@gmail.com>
2017-07-07 22:49:25 +0800

    Force WCA competition to enable cancellation

--- protected/models/Competition.php
+++ protected/models/Competition.php
@@ -1802,7 +1802,8 @@ class Competition extends ActiveRecord {
 	}
 
 	public function checkCancellationEnd() {
-		if ($this->cancellation_end_time > 0) {
+		//之前的比赛ID到669
+		if ($this->cancellation_end_time > 0 || $this->isWCACompetition() && $this->isAccepted() && $this->id > 669) {
 			if ($this->cancellation_end_time >= $this->reg_end - 86400) {
 				$this->addError('cancellation_end_time', '补报截止时间必须早于报名截止时间至少一天');
 			}
@@ -1810,15 +1811,20 @@ class Competition extends ActiveRecord {
 				$this->addError('cancellation_end_time', '补报截止时间必须晚于报名开始时间至少一周');
 			}
 		}
+		if ($this->cancellation_end_time == 0 xor $this->reg_reopen_time == 0) {
+			$this->addError('reg_reopen_time', '请同时设置补报截止时间和报名重开时间');
+			$this->addError('reg_reopen_time', '请同时设置补报截止时间和报名重开时间');
+		}
 	}
 
 	public function checkRegistrationReopen() {
-		if ($this->reg_reopen_time > 0) {
+		//之前的比赛ID到669
+		if ($this->reg_reopen_time > 0 || $this->isWCACompetition() && $this->isAccepted() && $this->id > 669) {
 			if ($this->reg_reopen_time >= $this->reg_end - 43200) {
-				$this->addError('reg_reopen_time', '报名截止时间必须早于比赛开始至少半天');
+				$this->addError('reg_reopen_time', '报名重开时间必须早于比赛开始至少半天');
 			}
 			if ($this->reg_reopen_time <= $this->cancellation_end_time + 43200) {
-				$this->addError('reg_reopen_time', '报名截止时间必须晚于比赛开始至少半天');
+				$this->addError('reg_reopen_time', '报名重开时间必须晚于比赛开始至少半天');
 			}
 		}
 	}
