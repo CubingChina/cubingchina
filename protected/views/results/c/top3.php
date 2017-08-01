6330b75a45ff14ee64f33915d7da1d909324efa8
Baiqiang <qiyuuu@gmail.com>
2017-08-01 13:55:00 +0800

    Change records style on competition page

--- protected/views/results/c/top3.php
+++ protected/views/results/c/top3.php
@@ -1,65 +1,53 @@
 <?php
-$this->widget('GroupGridView', array(
-  'dataProvider'=>new CArrayDataProvider($top3, array(
+$this->widget('GroupGridView', [
+  'dataProvider'=>new CArrayDataProvider($top3, [
     'pagination'=>false,
     'sort'=>false,
-  )),
+  ]),
   'itemsCssClass'=>'table table-condensed table-hover table-boxed',
   'groupKey'=>'eventId',
-  'groupHeader'=>'CHtml::link(Events::getFullEventNameWithIcon($data->eventId), array(
+  'groupHeader'=>'CHtml::link(Events::getFullEventNameWithIcon($data->eventId), [
     "/results/c",
     "id"=>$data->competitionId,
     "type"=>"all",
     "#"=>$data->eventId,
-  ))',
-  'columns'=>array(
-    array(
+  ])',
+  'columns'=>[
+    [
       'name'=>Yii::t('Results', 'Place'),
       'type'=>'raw',
       'value'=>'$data->pos',
-      'headerHtmlOptions'=>array('class'=>'place'),
-    ),
-    array(
+      'headerHtmlOptions'=>['class'=>'place'],
+    ],
+    [
       'name'=>Yii::t('Results', 'Person'),
       'type'=>'raw',
       'value'=>'Persons::getLinkByNameNId($data->personName, $data->personId)',
-    ),
-    array(
+    ],
+    [
       'name'=>Yii::t('common', 'Best'),
       'type'=>'raw',
-      'value'=>'$data->getTime("best")',
-      'headerHtmlOptions'=>array('class'=>'result'),
-      'htmlOptions'=>array('class'=>'result'),
-    ),
-    array(
-      'name'=>'',
-      'type'=>'raw',
-      'value'=>'$data->regionalSingleRecord',
-      'headerHtmlOptions'=>array('class'=>'record'),
-    ),
-    array(
+      'value'=>'$data->getTime("best", false, true)',
+      'headerHtmlOptions'=>['class'=>'result'],
+      'htmlOptions'=>['class'=>'result'],
+    ],
+    [
       'name'=>Yii::t('common', 'Average'),
       'type'=>'raw',
-      'value'=>'$data->getTime("average")',
-      'headerHtmlOptions'=>array('class'=>'result'),
-      'htmlOptions'=>array('class'=>'result'),
-    ),
-    array(
-      'name'=>'',
-      'type'=>'raw',
-      'value'=>'$data->regionalAverageRecord',
-      'headerHtmlOptions'=>array('class'=>'record'),
-    ),
-    array(
+      'value'=>'$data->getTime("average", false, true)',
+      'headerHtmlOptions'=>['class'=>'result'],
+      'htmlOptions'=>['class'=>'result'],
+    ],
+    [
       'name'=>Yii::t('common', 'Region'),
       'value'=>'Region::getIconName($data->person->country->name, $data->person->country->iso2)',
       'type'=>'raw',
-      'htmlOptions'=>array('class'=>'region'),
-    ),
-    array(
+      'htmlOptions'=>['class'=>'region'],
+    ],
+    [
       'name'=>Yii::t('common', 'Detail'),
       'type'=>'raw',
       'value'=>'$data->detail',
-    ),
-  ),
-)); ?>
+    ],
+  ],
+]); ?>
--- protected/views/results/c/winners.php
+++ protected/views/results/c/winners.php
@@ -1,62 +1,50 @@
 <?php
-$this->widget('GridView', array(
-  'dataProvider'=>new CArrayDataProvider($winners, array(
+$this->widget('GridView', [
+  'dataProvider'=>new CArrayDataProvider($winners, [
     'pagination'=>false,
     'sort'=>false,
-  )),
+  ]),
   'itemsCssClass'=>'table table-condensed table-hover table-boxed',
-  'columns'=>array(
-    array(
+  'columns'=>[
+    [
       'name'=>Yii::t('common', 'Event'),
       'type'=>'raw',
-      'value'=>'CHtml::link(Events::getFullEventNameWithIcon($data->eventId), array(
+      'value'=>'CHtml::link(Events::getFullEventNameWithIcon($data->eventId), [
         "/results/c",
         "id"=>$data->competitionId,
         "type"=>"all",
         "#"=>$data->eventId,
-      ))',
-    ),
-    array(
+      ])',
+    ],
+    [
       'name'=>Yii::t('Results', 'Person'),
       'type'=>'raw',
       'value'=>'Persons::getLinkByNameNId($data->personName, $data->personId)',
-    ),
-    array(
+    ],
+    [
       'name'=>Yii::t('common', 'Best'),
       'type'=>'raw',
-      'value'=>'$data->getTime("best")',
-      'headerHtmlOptions'=>array('class'=>'result'),
-      'htmlOptions'=>array('class'=>'result'),
-    ),
-    array(
-      'name'=>'',
-      'type'=>'raw',
-      'value'=>'$data->regionalSingleRecord',
-      'headerHtmlOptions'=>array('class'=>'record'),
-    ),
-    array(
+      'value'=>'$data->getTime("best", false, true)',
+      'headerHtmlOptions'=>['class'=>'result'],
+      'htmlOptions'=>['class'=>'result'],
+    ],
+    [
       'name'=>Yii::t('common', 'Average'),
       'type'=>'raw',
-      'value'=>'$data->getTime("average")',
-      'headerHtmlOptions'=>array('class'=>'result'),
-      'htmlOptions'=>array('class'=>'result'),
-    ),
-    array(
-      'name'=>'',
-      'type'=>'raw',
-      'value'=>'$data->regionalAverageRecord',
-      'headerHtmlOptions'=>array('class'=>'record'),
-    ),
-    array(
+      'value'=>'$data->getTime("average", false, true)',
+      'headerHtmlOptions'=>['class'=>'result'],
+      'htmlOptions'=>['class'=>'result'],
+    ],
+    [
       'name'=>Yii::t('common', 'Region'),
       'value'=>'Region::getIconName($data->person->country->name, $data->person->country->iso2)',
       'type'=>'raw',
-      'htmlOptions'=>array('class'=>'region'),
-    ),
-    array(
+      'htmlOptions'=>['class'=>'region'],
+    ],
+    [
       'name'=>Yii::t('common', 'Detail'),
       'type'=>'raw',
       'value'=>'$data->detail',
-    ),
-  ),
-)); ?>
+    ],
+  ],
+]); ?>
