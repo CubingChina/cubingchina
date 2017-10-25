386b81344dbc1f4ba8147e7bb45ded25e7f75604
Baiqiang <qiyuuu@gmail.com>
2017-10-25 17:45:30 +0800

    Reorganize configuration files

--- protected/config/console.php
+++ protected/config/console.php
@@ -1,17 +1,13 @@
 <?php
-define('APP_PATH', dirname(dirname(__DIR__)));
-define('ENV', isset($_SERVER['ENV']) ? $_SERVER['ENV'] : 'production');
-define('DEV', ENV === 'dev');
-if (is_file($autoload = APP_PATH . '/protected/vendor/autoload.php')) {
-	require $autoload;
-}
-$config = require APP_PATH . '/protected/config/main.php';
+
+include __DIR__ . '/bootstrap.php';
+$config = require CONFIG_PATH . '//main.php';
 unset($config['components']['log']['routes'][count($config['components']['log']['routes']) - 1]);
-$config['commandMap'] = array(
-	'migrate'=>array(
+$config['commandMap'] = [
+	'migrate'=>[
 		'class'=>'system.cli.commands.MigrateCommand',
 		'migrationTable'=>'migration',
 		'interactive'=>false,
-	),
-);
+	],
+];
 return $config;
