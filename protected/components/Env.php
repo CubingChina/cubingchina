3fa02f4bd7371b85713c9207ab2f3845e588b883
Baiqiang <qiyuuu@gmail.com>
2017-11-06 16:55:08 +0800

    Change env getter

--- protected/components/Env.php
+++ protected/components/Env.php
@@ -3,8 +3,22 @@
 class Env {
 	const PREFIX = 'CUBINGCHINA_';
 
+	private static $envs;
+
 	public static function get($name) {
 		$key = self::PREFIX . $name;
-		return isset($_SERVER[$key]) ? $_SERVER[$key] : '';
+		$envs = self::getEnvs();
+		return $_SERVER[$key] ?? $envs[$key] ?? '';
+	}
+
+	public static function getEnvs() {
+		if (self::$envs === null) {
+			self::$envs = [];
+			$envFile = CONFIG_PATH . '/env.php';
+			if (file_exists($envFile)) {
+				self::$envs = require $envFile;
+			}
+		}
+		return self::$envs;
 	}
 }
