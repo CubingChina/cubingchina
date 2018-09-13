d2311c1522e181904ef9b608ff5c1d98d9b44661
Baiqiang <qiyuuu@gmail.com>
2018-09-13 21:25:56 +0800

    Clear useless code

--- protected/commands/RegistrationCommand.php
+++ protected/commands/RegistrationCommand.php
@@ -1,9 +1,6 @@
 <?php
-use \Yunpian\Sdk\YunpianClient;
 
 class RegistrationCommand extends CConsoleCommand {
-
-	private $_smsClient;
 
 	public function actionClearWaitingList() {
 		$competitions = Competition::model()->findAllByAttributes([
@@ -56,41 +53,5 @@ class RegistrationCommand extends CConsoleCommand {
 
 	private function logDisqualified($registration, $disqualifiedEvents, $disqualified) {
 		echo implode("\t", [$registration->user->getCompetitionName(), $registration->user->wcaid, $disqualified ? 'disqualified' : '', implode("\t", $disqualifiedEvents)]), "\n";
-		if ($disqualified) {
-			$this->sendSms($registration->user->mobile, "【粗饼网】尊敬的{$registration->user->name_zh}，2018中国魔方锦标赛参赛资格筛查已完成，很抱歉的通知您，由于您所有项目均未达到资格线要求，您的报名已被取消，无法参加本次比赛，详见规则：http://t.cn/Rssv1Ou");
-		} else {
-			$this->sendSms($registration->user->mobile, sprintf('【粗饼网】尊敬的%s，2018中国魔方锦标赛参赛资格筛查已完成，很抱歉的通知您，由于您报名的项目%s未达到资格线要求，无法参加本次比赛的上述项目，详见规则：http://t.cn/Rssv1Ou',
-				$registration->user->name_zh,
-				implode('、', array_map(function($event) {
-					if (is_object($event) && isset($event->event)) {
-						$event = $event->event;
-					}
-					return Events::getFullEventName($event);
-				}, $disqualifiedEvents))
-			));
-		}
-	}
-
-	private function sendSms($mobile, $message) {
-		if (!preg_match('|^1\d{10}$|', $mobile)) {
-			return;
-		}
-		$mobile = '18601371730';
-		$client = $this->getSmsClient();
-		$result = $client->single_send([
-			YunpianClient::MOBILE=>$mobile,
-			YunpianClient::TEXT=>$message,
-		]);
-		if (!$result->isSucc()){
-			Yii::log("Send to {$mobile} failed. Message: {$message}", 'error', 'sms');
-		}
-		return $result->isSucc();
-	}
-
-	private function getSmsClient() {
-		if ($this->_smsClient === null) {
-			$this->_smsClient = YunpianClient::create(Env::get('YUNPIAN_API_KEY'));
-		}
-		return $this->_smsClient->sms();
 	}
 }
