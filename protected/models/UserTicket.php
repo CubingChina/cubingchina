ff6af17c05340054d96518cd1ee7977c8b7e0694
Baiqiang <qiyuuu@gmail.com>
2018-09-28 18:10:31 +0800

    Disqualified competitor can get discount

--- protected/models/UserTicket.php
+++ protected/models/UserTicket.php
@@ -72,7 +72,7 @@ class UserTicket extends ActiveRecord {
 			$competition = $this->ticket->competition;
 		}
 		$registration = Registration::getUserRegistration($competition->id, $user->id);
-		return $registration !== null && $registration->isAccepted() && $competition->date - $user->birthday <= 12 * 365.25 * 86400 && self::model()->countByAttributes([
+		return $registration !== null && ($registration->isAccepted() || $registration->isDisqualified()) && $competition->date - $user->birthday <= 12 * 365.25 * 86400 && self::model()->countByAttributes([
 			'ticket_id'=>$competition->getTicketIds(),
 			'user_id'=>$this->user_id,
 		], [
