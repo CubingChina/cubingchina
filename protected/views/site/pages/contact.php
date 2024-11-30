c88771affc5d8322702281e2110121e8d0e54794
Jiashun Wu <jiashunwu2001@163.com>
2024-11-30 18:58:07 +0800

    feat: change contact infomation

--- protected/views/site/pages/contact.php
+++ protected/views/site/pages/contact.php
@@ -9,29 +9,39 @@
   <?php echo Yii::app()->controller->translateTWInNeed(
 <<<EOT
   <p>
-    如果你想采用我们的比赛管理系统并公示一场魔方竞速比赛，或者给我们网站以更多更好的意见和建议，欢迎通过以下邮件联系我们：
+    如果你想给我们网站以更多更好的意见和建议，欢迎通过以下邮件联系我们：
     <br><i class="fa fa-envelope"></i> <a href="mailto:admin@cubingchina.com">admin@cubingchina.com</a>。
   </p>
   <p>
-    如果你想举办一场WCA官方魔方比赛或举办相关活动，可以联系中国的世界魔方协会代表(WCA Delegates)：
-    <br><i class="fa fa-envelope"></i> <a href="mailto:wcadaibiao@gmail.com">wcadaibiao@gmail.com</a>。
+    如果你想在粗饼平台申请并公示一场魔方比赛，或提交与比赛相关的材料（如主办方申请文件、团队介绍等），请联系粗饼赛事审核组：
+    <br><i class="fa fa-envelope"></i> <a href="mailto:competitions@cubing.com">competitions@cubing.com</a>。
   </p>
   <p>
-    如果你的所在地位于或者邻近以下城市列表中，你也可以直接与该地的世界魔方协会代表取得联系：
+    如果你想要咨询世界魔方协会相关的事宜，请联系世界魔方协会通讯组中国团队：
+    <br><i class="fa fa-envelope"></i> <a href="mailto:wct-china@worldcubeassociation.org">wct-china@worldcubeassociation.org</a>。
   </p>
+  <p>
+    如果你对于比赛成绩或比赛规则有疑问，可以联系中国的世界魔方协会代表(WCA Delegates)的公共邮箱：
+    <br><i class="fa fa-envelope"></i> <a href="mailto:wcadaibiao@gmail.com">wcadaibiao@gmail.com</a>。
+    <br>或者你想直接具体和某位世界魔方协会代表取得联系，可以直接通过下面的联系方式：
+  </p>
+
   <p>
   </p>
 EOT
 ); ?>
   <?php else: ?>
   <p>
-    If you would like to use our website to announce a speedcubing competition and generate a competition webpage, or have any suggestions, feel free to contact the <u>Cubing China Administrator</u> via <br><i class="fa fa-envelope"></i> <a href="mailto:admin@cubingchina.com">admin@cubingchina.com</a>.
+    If you want to provide feedback and suggestions, feel free to contact the Cubing Administrator via admin@cubingchina.com. 
   </p>
   <p>
-    If you would like to hold a WCA official speedcubing competition or organize a relevant acitivity, please contact the <u>Chinese WCA Delegates</u> via <br><i class="fa fa-envelope"></i> <a href="mailto:wcadaibiao@gmail.com">wcadaibiao@gmail.com</a>.
+    If you would like to use our website to announce a speedcubing competition and generate a competition webpage or submit your organization team applications, please contact the Cubing Competition Team via competitions@cubing.com. 
   </p>
   <p>
-    If you are located in or near one of the following cities, you can also get in touch with the local WCA Delegate directly.
+    If you have questions regarding the World Cube Association, please contact the WCA Communication China team via wct-china@worldcubeassociation.org  
+  </p>
+  <p>
+    If you have questions regarding specific competitions or regulations, please contact the Chinese WCA Delegates via wcadaibiao@gmail.com. Alternatively, one can get in touch with one of the delegates directly via the contact info below:
   </p>
   <p>
   </p>
