b68e1f8c0845f5ddf21efef0369a01f9c0b3a38d
Jiashun Wu <jiashunwu2001@163.com>
2025-07-10 22:07:08 +0800

    fix: add migration for competition_explanation

---
+++ protected/migrations/m250710_140631_competition_explanation.php
@@ -0,0 +1,25 @@
+<?php
+
+class m250710_140631_competition_explanation extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m250710_140631_competition_explanation does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
