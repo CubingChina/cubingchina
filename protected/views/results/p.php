c159572f018d22507c04c9be8666f06c6001d828
Baiqiang <qiyuuu@gmail.com>
2026-01-02 17:28:52 +0800

    feat: add icons

--- protected/views/results/p.php
+++ protected/views/results/p.php
@@ -32,7 +32,7 @@
         </div>
         <div class="col-md-4 col-sm-6 col-xs-12 mt-10">
           <span class="info-title"><?php echo Yii::t('Results', 'Career'); ?>:</span>
-          <span class="info-value"><?php echo sprintf('%d.%02d.%02d - %d.%02d.%02d', $firstCompetition->year, $firstCompetition->month, $firstCompetition->day, $lastCompetition->year, $lastCompetition->endMonth, $lastCompetition->endDay); ?></span>
+          <span class="info-value"><?php echo sprintf('%d.%02d.%02d - %d.%02d.%02d', $firstCompetition->year, $firstCompetition->month, $firstCompetition->day, ($lastCompetition->endMonth < $lastCompetition->month || $lastCompetition->endDay < $lastCompetition->day ? $lastCompetition->year + 1 : $lastCompetition->year), $lastCompetition->endMonth, $lastCompetition->endDay); ?></span>
         </div>
       </div>
     </div>
